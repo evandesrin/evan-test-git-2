a3cdffc830aba285587f5ea08055d0a3008073d2
Joseph Evan <jedp121206@gmail.com>
2020-04-27 10:30:36 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
   <h1>hello World</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nam mollitia illo voluptatibus fuga minus, nisi quibusdam, laudantium cum numquam dicta. Dicta delectus est quasi illum odit harum repellendus distinctio?</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+    <li>tiga</li>
+  </ul>
 </body>
 </html>
